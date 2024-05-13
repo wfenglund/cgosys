f8b1204b7c7081eaa5a4791cd1e93c99d033cb0d
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-05-13 02:26:41 +0200

    add support for controlling the menu with a controller

--- cgosys.py
+++ cgosys.py
@@ -1,11 +1,30 @@
 import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1' # to make pygame import silent
+import pygame
 import subprocess
 import curses
 
-def detect_games(rom_path):
+pygame.init()
+controllers = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+
+def get_contr_press():
+    if len(controllers) > 0:
+        controllers[0].init()
+        button_dict = {12:258, 11:259, 0:10}
+        events = pygame.event.get()
+        if len(events) > 0:
+            for press in events:
+                if 'button' in press.dict.keys():
+                    button = press.dict['button']
+                    button = button_dict[button] if button in button_dict.keys() else ''
+                    controllers[0].quit()
+                    return button
+    return ''
+
+def detect_games(rom_path, game_type, ending):
     file_list = os.listdir(rom_path)
     game_dict = {}
-    game_dict['gba'] = [i for i in file_list if i.endswith('.gba')]
+    game_dict[game_type] = [i for i in file_list if i.endswith(ending)]
     return game_dict
 
 device = 'PowerA'
@@ -18,9 +37,10 @@ config_dict['keyboard'] = 'keyboard_vbam.cfg'
 device_config = config_dict[device]
 
 gba_roms_path = './gba_roms/'
-roms = detect_games(gba_roms_path)
+roms = detect_games(gba_roms_path, 'gba', '.gba')
 
 def cgosys_menu(stdscr):
+    stdscr.nodelay(True)
     attributes = {}
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -40,6 +60,10 @@ def cgosys_menu(stdscr):
                     attr = attributes['normal']
                 stdscr.addstr(choices[i] + '\n', attr)
             character = stdscr.getch()
+            if character == -1:
+                joypress = get_contr_press()
+                if joypress != '':
+                    character = joypress
             if character == curses.KEY_UP and option > 0:
                 option -= 1
             elif character == curses.KEY_DOWN and option < len(choices) - 1:
