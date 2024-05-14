459c1e6e732cf7c999647678eada432d24a0856a
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-05-15 01:52:59 +0200

    added comments for clarification and structure

--- cgosys.py
+++ cgosys.py
@@ -1,12 +1,15 @@
+### Packages:
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1' # to make pygame import silent
 import pygame
 import subprocess
 import curses
 
+### Initiate pygame find controllers:
 pygame.init()
 controllers = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 
+### Functions:
 def detect_games(rom_path, game_type, ending):
     file_list = os.listdir(rom_path)
     game_dict = {}
@@ -28,7 +31,7 @@ def get_contr_press():
     return ''
 
 def cgosys_menu(stdscr):
-    stdscr.nodelay(True)
+    stdscr.nodelay(True) # do not for .getch()
     attributes = {}
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -41,22 +44,22 @@ def cgosys_menu(stdscr):
         while character != 10: # while Enter has not been pressed
             stdscr.erase()
             stdscr.addstr("Select game to play:\n", curses.A_UNDERLINE)
-            for i in range(len(choices)):
+            for i in range(len(choices)): # paint menu choices
                 if i == option:
                     attr = attributes['highlighted']
                 else:
                     attr = attributes['normal']
                 stdscr.addstr(choices[i] + '\n', attr)
             character = stdscr.getch()
-            if character == -1:
+            if character == -1: # if no key was pressed
                 joypress = get_contr_press()
-                if joypress != '':
+                if joypress != '': # if controller has been pressed
                     character = joypress
             if character == curses.KEY_UP and option > 0:
                 option -= 1
             elif character == curses.KEY_DOWN and option < len(choices) - 1:
                 option += 1
-        choice = choices[option]
+        choice = choices[option] # get menu choice
         if choice == 'Quit':
             break
         else:
@@ -64,8 +67,9 @@ def cgosys_menu(stdscr):
             print(message, end = '\r')
             devnull = subprocess.DEVNULL
             subprocess.run(['vbam', '-c', device_config, f'{gba_roms_path}{choice}'], stdout = devnull, stderr = devnull)
-            print(' ' * len(message), end = '\r')
+            print(' ' * len(message), end = '\r') # clean avay message
 
+### Determine VBAM settings:
 device = 'PowerA'
 # device = 'keyboard'
 
@@ -75,9 +79,11 @@ config_dict['keyboard'] = 'keyboard_vbam.cfg'
 
 device_config = config_dict[device]
 
+### Detect GBA games:
 gba_roms_path = './gba_roms/'
 roms = detect_games(gba_roms_path, 'gba', '.gba')
 
+### Spawn subprocess for quitting VBAM from within, and start cgosys menu:
 kill_proc = subprocess.Popen(['python3', './kill_process.py'])
 curses.wrapper(cgosys_menu)
 kill_proc.kill()
